52e058d7cfde044ffe756dd591655382d32cad95
Alex Kahan <alex@zerofox.com>
2016-04-30 12:21:55 -0400

    Implementation of client and cache decorator

---
+++ redis_cache.py
@@ -0,0 +1,46 @@
+from redis_client import RedisClient
+
+
+class RedisCache(object):
+    def __init__(self, address, port):
+        self.address = address
+        self.port = port
+        self.redis_client = RedisClient(self.address, self.port)
+
+    def cache(self, fn):
+        """
+        Cache decorator
+        """
+        def wrapper(*args, **kwargs):
+            fn_name = fn.__name__
+            signature = self._get_signature(args, **kwargs)
+            fn_hash = str(hash(fn_name + signature))
+            cache_request = self.redis_client.get(fn_hash)
+
+            if cache_request is '':
+                # Cache miss
+                ret = fn(*args, **kwargs)
+                self.redis_client.set(fn_hash, ret)
+            else:
+                # Cache hit
+                return cache_request
+            return ret
+        return wrapper
+
+    def _get_signature(*args, **kwargs):
+        """
+        Gets the signature of the decorated method
+        :return: arg1,...argn,kwarg1=kwarg1,...kwargn=kwargn
+        """
+        # Join regular arguments together with commas
+        parsed_args = ",".join(map(lambda x: str(x), args[1]))
+        # Join keyword arguments together with `=` and commas
+        parsed_kwargs = ",".join(
+            map(lambda x: '%s=%s' % (x, str(kwargs[x])), kwargs)
+        )
+        # Filter out empty params
+        parsed = filter(
+            lambda x: x != '', [parsed_args, parsed_kwargs]
+        )
+
+        return ','.join(parsed)
---
+++ redis_client.py
@@ -0,0 +1,66 @@
+import socket
+
+
+class RedisClient(object):
+    """
+    Client to communicate with the Redis Server
+    """
+
+    def __init__(self, address, port):
+        self.delimiter = '\r\n'
+        self.address = address
+        self.port = port
+        self.RECV_SIZE = 2048
+
+    def _make_request(self, command):
+        """
+        Makes the request to the Redis Server
+        :param command: Raw Redis command
+        :return: Response from Redis Server
+        """
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.address, int(self.port)))
+        s.send(command)
+        response = s.recv(self.RECV_SIZE)
+        s.close()
+        return response
+
+    def _build_command(self, *args):
+        """
+        Builds a Redis command
+        :return: Raw Redis command
+        """
+        command_args = ['*%s' % len(args)]
+        for arg in args:
+            command_args.extend(['$%s' % len(str(arg)), str(arg)])
+        return self.delimiter.join(command_args) + self.delimiter
+
+    def get(self, key):
+        """
+        GET method
+        :param key: The key to GET
+        """
+        command = self._build_command('GET', key)
+        response = self._make_request(command)
+        return response.split(self.delimiter)[1]
+
+    def set(self, key, value):
+        """
+        SET method
+        :param key: The key to SET
+        :param value: The value of the key to SET
+        """
+        command = self._build_command('SET', key, value)
+        response = self._make_request(command)
+        return response.split(self.delimiter)[0]
+
+    def setex(self, key, value, expiration):
+        """
+        SETEX command
+        :param key: The key to SET
+        :param value: The value of the key to SET
+        :param expiration: TTL in seconds
+        """
+        command = self._build_command('SETEX', key, expiration, value)
+        response = self._make_request(command)
+        return response.split(self.delimiter)[0]
