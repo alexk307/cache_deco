b7624e478887e8ba994826a46fa8c2d351b8e233
Alex Kahan <alex@zerofox.com>
2016-05-03 21:27:49 -0400

    pypi stuff

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='redis_cache_decorator',
+    packages=['redis_cache'],
+    version='0.1.0',
+    description='Implements function level caching to Redis with a decorator',
+    author='Alex Kahan',
+    author_email='alexk307@gmail.com',
+    url='https://github.com/alexk307/redis_cache',
+    download_url='https://github.com/alexk307/redis_cache',
+    keywords=['redis', 'cache'],
+    classifiers=[],
+)
