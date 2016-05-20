df190a565bda766b35d8067326833e6837e28e83
Alex Kahan <alex@zerofox.com>
2016-05-19 22:42:51 -0400

    Uploading new version to PyPi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='redis_cache_decorator',
     packages=['redis_cache'],
-    version='0.1.1',
+    version='0.2.0',
     description='Implements function level caching to Redis with a decorator',
     author='Alex Kahan',
     author_email='alexk307@gmail.com',
